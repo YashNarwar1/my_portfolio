5f9b29cb8d9c063deb94689609cc1651d545297e
Yash Narwar <yashnarwar111@gmail.com>
2025-04-06 04:13:03 +0530

    wieght change

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -90,7 +90,7 @@ export const Card = ({
             transition: { duration: 0.6, ease: "easeOut" },
           },
         }}
-        className="w-[60%] flex flex-col gap-4 lg:hidden absolute bottom-10 left-10">
+        className="w-[90%] flex flex-col gap-4 lg:hidden absolute bottom-10 left-10">
         <h1 className="text-4xl font-bold tracking-wider text-slate-100 md:text-slate-800">
           {title}
         </h1>
@@ -113,43 +113,3 @@ export const Card = ({
     </div>
   );
 };
-
-// import { cn } from "../lib/utils";
-
-// export const Card = ({
-//   className,
-//   classNameHeader,
-//   imageUrl,
-//   title,
-//   description,
-//   height,
-// }) => {
-//   return (
-//     <div
-//       className={cn(
-//         "w-full  rounded-xl flex flex-col justify-center gap-2 mt-4  px-3 grayscale-70 mb-10 lg:mb-0 ",
-//         height
-//       )}>
-//       <div
-//         className={cn(
-//           "flex items-center justify-center   px-10  rounded-3xl bg-[url('/bg.jpg')] bg-cover filter-grayscale hover:filter-none",
-//           className
-//         )}>
-//         <div
-//           className="w-full h-[80%] md:h-[90%] rounded-3xl  bg-cover bg-no-repeat"
-//           style={{ backgroundImage: `url(${imageUrl})` }}
-//         />
-//       </div>
-
-//       <h1
-//         className={cn(
-//           "flex items-center gap-4  font-bold text-black pt-2",
-//           classNameHeader
-//         )}>
-//         {title}
-//       </h1>
-
-//       <p className="text-lg text-slate-500 ">{description}</p>
-//     </div>
-//   );
-// };
